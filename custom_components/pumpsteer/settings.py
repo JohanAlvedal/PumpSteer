c1b11601a8797c42b76bfd8de9edfb2d9e2bc62f
AlvJo <goktytan@gmail.com>
2025-07-05 18:22:22 +0200

    Create settings.py
    Created

---
+++ custom_components/pumpsteer/settings.py
@@ -0,0 +1,15 @@
+# Temperaturinställningar
+COMFORT_TEMP_TARGET = 21.0
+SUMMER_TEMP_THRESHOLD = 17.5
+HOLIDAY_TEMP = 16.0
+
+# Elprisinställningar
+HIGH_PRICE_THRESHOLD = 2.00  # kronor/kWh
+LOW_PRICE_THRESHOLD = 0.80
+
+# Tröghetsvärden (husets respons på tempförändringar)
+DEFAULT_HOUSE_INERTIA = 3  # timmar
+MAX_PREBOOST_HOURS = 6
+
+# Datumformat för semester
+DATE_FORMAT = "%Y-%m-%d"
