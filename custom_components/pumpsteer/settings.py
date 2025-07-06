ff5d5990af77ff2b361d5be40acfea12ca3b24fa
AlvJo <goktytan@gmail.com>
2025-07-06 08:54:23 +0200

    Update settings.py

--- custom_components/pumpsteer/settings.py
+++ custom_components/pumpsteer/settings.py
@@ -13,3 +13,17 @@ MAX_PREBOOST_HOURS = 6
 
 # Datumformat för semester
 DATE_FORMAT = "%Y-%m-%d"
+
+# Pre_boost Inställningar
+
+# Maximal pre-boostjustering i grader
+MAX_PREBOOST_ADJUSTMENT = 5.0  # °C
+
+# Minsta elpris som triggar pre-boost
+PREBOOST_PRICE_THRESHOLD = 1.20  # SEK/kWh
+
+# Minsta temperaturprognos som kräver pre-boost
+PREBOOST_TEMP_THRESHOLD = 2.0  # °C
+
+# Tröghetsmultiplikator för att skala pre-boostens tid
+INERTIA_SCALING_FACTOR = 1.0  # kan justeras för att påverka hur långt i förväg pre-boost triggas
