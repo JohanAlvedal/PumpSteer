6aecb39ffe39aba9bb4ab4f92aef236ddaf21c29
AlvJo <goktytan@gmail.com>
2025-07-06 10:34:14 +0200

    Update settings.py

--- custom_components/pumpsteer/settings.py
+++ custom_components/pumpsteer/settings.py
@@ -10,7 +10,7 @@ MAX_PREBOOST_HOURS = 6           # How many hours ahead to look for pre-boost
 PREBOOST_TEMP_THRESHOLD = 2.0    # How many degrees colder than target to trigger pre-boost
 PREBOOST_PRICE_THRESHOLD = 1.20  # SEK/kWh - Absolute price threshold for pre-boost
 
-# New additions:
+# Braking and output settings
 BRAKING_MODE_TEMP = 25.0         # Virtual outdoor temperature when braking due to high price
 PREBOOST_OUTPUT_TEMP = -15.0     # Virtual outdoor temperature when pre-boosting
 
@@ -18,3 +18,6 @@ NORMAL_MODE_MAX_OUTPUT_TEMP = 20.0 # Max virtual outdoor temp in normal operatio
 NORMAL_MODE_MIN_OUTPUT_TEMP = -10.0 # Min virtual outdoor temp in normal operation
 
 AGGRESSIVENESS_SCALING_FACTOR = 0.5 # Factor for aggressiveness in normal mode
+
+# Pre-boost strategy settings (for pre_boost.py)
+COLD_HOUR_TEMP_THRESHOLD = 18.0 # Max temperature for an hour to be considered "cold" for pre-boost strategy
