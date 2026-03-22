d7854522f26883382e124b8672c9ed588b5d45ec
AlvJo <goktytan@gmail.com>
2026-03-22 17:21:22 +0100

    Gate ML behind experimental opt-in option

--- custom_components/pumpsteer/sensor/__init__.py
+++ custom_components/pumpsteer/sensor/__init__.py
@@ -2,8 +2,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .sensor import PumpSteerSensor
-from .ml_sensor import PumpSteerMLSensor
+from .sensor import PumpSteerSensor, is_ml_experimental_enabled
 
 
 async def async_setup_entry(
@@ -13,5 +12,9 @@ async def async_setup_entry(
 ) -> None:
     """Register both control and ML analysis sensors"""
     sensor = PumpSteerSensor(hass, config_entry)
-    ml_sensor = PumpSteerMLSensor(hass, config_entry)
-    async_add_entities([sensor, ml_sensor], update_before_add=True)
+    entities = [sensor]
+    if is_ml_experimental_enabled(config_entry):
+        from .ml_sensor import PumpSteerMLSensor
+
+        entities.append(PumpSteerMLSensor(hass, config_entry))
+    async_add_entities(entities, update_before_add=True)
