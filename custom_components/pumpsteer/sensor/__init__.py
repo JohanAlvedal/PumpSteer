670afc9171074f081edb1c94fa302c66b27ad179
AlvJo <goktytan@gmail.com>
2026-03-23 06:35:40 +0100

    Simplify sensor setup by removing ML sensor

--- custom_components/pumpsteer/sensor/__init__.py
+++ custom_components/pumpsteer/sensor/__init__.py
@@ -3,7 +3,6 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .sensor import PumpSteerSensor
-from .ml_sensor import PumpSteerMLSensor
 
 
 async def async_setup_entry(
@@ -11,7 +10,5 @@ async def async_setup_entry(
     config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-    """Register both control and ML analysis sensors"""
-    sensor = PumpSteerSensor(hass, config_entry)
-    ml_sensor = PumpSteerMLSensor(hass, config_entry)
-    async_add_entities([sensor, ml_sensor], update_before_add=True)
+    """Set up PumpSteer sensor."""
+    async_add_entities([PumpSteerSensor(hass, config_entry)], update_before_add=True)
