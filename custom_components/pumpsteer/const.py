2ebce656778570c58f465a03df779084ea1f19e0
AlvJo <goktytan@gmail.com>
2026-09-08 11:29:39 +0200

    Bump V3 alpha for configurable sensors

--- custom_components/pumpsteer/const.py
+++ custom_components/pumpsteer/const.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
     from .v3.ha.runtime import PumpSteerRuntime
 
 DOMAIN = "pumpsteer"
-INTEGRATION_VERSION = "3.0.0-alpha.2"
+INTEGRATION_VERSION = "3.0.0-alpha.3"
 PLATFORMS = ("climate", "number")
 
 CONF_INDOOR_ENTITY = "indoor_temp_entity"
